d696e74fc0cbb9c5e185207c7f2faa2f06af65e6
mahamudul hasan seber <sebermh@gmail.com>
2026-03-14 00:59:04 +0600

    Create Pages + Folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import { RouterProvider } from "react-router/dom";
+import { router } from "./Routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
