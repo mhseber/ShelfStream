801284392ac7d6ef0706fa29e8a8752a182e816d
mahamudul hasan seber <sebermh@gmail.com>
2026-03-14 00:48:03 +0600

    Page SetUp Add Daisy UI, TailwindCss, React Router

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <div>
+        <button className="btn btn-outline">Default</button>
+        <button className="btn btn-outline btn-primary">Primary</button>
+        <button className="btn btn-outline btn-secondary">Secondary</button>
+        <button className="btn btn-outline btn-accent">Accent</button>
+        <button className="btn btn-outline btn-info">Info</button>
+        <button className="btn btn-outline btn-success">Success</button>
+        <button className="btn btn-outline btn-warning">Warning</button>
+        <button className="btn btn-outline btn-error">Error</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
